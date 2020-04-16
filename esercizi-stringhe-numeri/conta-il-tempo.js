1df73d7052228c5222107f388942ee9a661b2c2b
cristiancapri99 <cristiancapri99@users.noreply.github.com>
2020-04-16 22:34:02 +0200

    yes

--- esercizi-stringhe-numeri/conta-il-tempo.js
+++ esercizi-stringhe-numeri/conta-il-tempo.js
@@ -1,8 +1,20 @@
-var minuti = 60 ;
+/*
+  Conta il tempo
+  Scrivi un programma che dato un numero di secondi, calcoli la quantità di ore, minuti e secondi corrispondenti e
+  poi stampi il risultato. L'output avrà solo numeri interi.
+  Esempio:
+    Input: 12560
+    Output: 3 ore, 29 minuti e 20 secondi.
+  Consigli:
+  In un'ora ci sono 60 minuti, in un minuto 60 secondi. Quindi quanti secondi ci sono in un'ora? ;)
+  http://www.imparareaprogrammare.it
+*/
 
-var secondi = 60 ;
 
-var secInHour = minuti * secondi;
+var ora = 60
 
+var minuto = 60
 
-console.log(n.toString(secInHour));
+var secInHour = ora*minuto;
+
+console.log(secInHour);
--- esercizi-stringhe-numeri/gatti-in-fila.js
+++ esercizi-stringhe-numeri/gatti-in-fila.js
@@ -1,3 +1,40 @@
+/*
+  Gatti in fila
+  Scrivi un programma che dato:
+    a) Un numero totale di gatti
+    b) Il numero dei gatti presenti in ogni fila
+  Restituisca in output:
+    - Il numero di file risultanti
+    - Nel caso l'ultima fila risulti incompleta indicare il numero di gatti mancanti per completarla (quanti gatti mancano per arrivare a b?)
+  Esempio:
+    Input: numero di gatti = 44, gatti per fila = 6
+    Output: numero di file = 8, gatti mancanti = 4
+  Consigli:
+  Per l'arrotondamento ti consiglio di non usare Math.round (arrotonda all'intero superiore solo da .5 in su),
+  utilizza Math.ceil (arrotonda sempre all'intero superiore).
+  http://www.imparareaprogrammare.it
+*/
+
+
+var cats = 44
+
+var inLines = 6 
+
+var Lines = cats/inLines;
+
+var LinesRounded = Math.ceil(Lines);
+
+console.log(LinesRounded);
+
+var necessary = LinesRounded*inLines-cats
+
+console.log(necessary);
+
+
+
+oppure:
+
+
 var cats = 44;
 
 var inLine = 6;
@@ -7,4 +44,3 @@ var file = Math.ceil (cats/inLine);
  var missing = file*inLine-cats;
 
 console.log((cats) in fila per (inLine) si unirono compatti in (file). Nell'ultima fila rimasero (missing) posti gatto disponibili);
-    
